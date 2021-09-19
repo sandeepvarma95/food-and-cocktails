94fd418b56efee71572afa742d5c048c60da7db2
Sandeep Varma <67902146+sandeepvarma95@users.noreply.github.com>
2021-09-19 18:03:45 -0400

    Updated Commit -- Food Menu and Cocktail Page
    Food Menu page updated and pie chart added. 
    Firebase Database up to date.
    Cocktail page designed and updated.

--- src/Cocktail.js
+++ src/Cocktail.js
@@ -1,8 +1,73 @@
-import React,{useState, useEffect} from 'react'
+import db from "./firebaseConfig.js";
+import React, { useState, useEffect } from "react";
+import { PieChart } from "react-minimal-pie-chart";
+import "./Cocktail.css";
 
 function Cocktail() {
+  const [info, setInfo] = useState([]);
 
-   
+  // Start the fetch operation as soon as
+  // the page loads
+  window.addEventListener("load", () => {
+    Fetchdata();
+  });
+
+  // Fetch the required data using the get() method
+  const Fetchdata = () => {
+    db.collection("cocktailData")
+      .orderBy("name")
+      .get()
+      .then((querySnapshot) => {
+        // Loop through the data and store
+        // it in array to display
+        querySnapshot.forEach((element) => {
+          var data = element.data();
+          setInfo((arr) => [...arr, data]);
+        });
+      });
+  };
+
+  return (
+    <div className="grid">
+      {info &&
+        info.map((foo) => {
+          return (
+            <div className="food_container">
+              <div className="img_container">
+                <img className="food_image" src={foo.img} alt="" />
+              </div>
+              <div className="data_container">
+                <h1>{foo.name}</h1>
+                <p>
+                  {" "}
+                  {foo.con1} <b>{foo.quan1}</b>{" "}
+                </p>
+                <p>
+                  {" "}
+                  {foo.con2} <b>{foo.quan2}</b>{" "}
+                </p>
+                <p>
+                  {" "}
+                  {foo.con3} <b>{foo.quan3}</b>{" "}
+                </p>
+                <p>
+                  {" "}
+                  {foo.con4} <b>{foo.quan4}</b>{" "}
+                </p>
+                <p>
+                  {" "}
+                  {foo.con5} <b>{foo.quan5}</b>{" "}
+                </p>
+                <p>
+                  {" "}
+                  {foo.con6} <b>{foo.quan6}</b>{" "}
+                </p>
+              </div>
+            </div>
+          );
+        })}
+    </div>
+  );
 }
 
-export default Cocktail
+export default Cocktail;
--- src/Food.js
+++ src/Food.js
@@ -1,46 +1,69 @@
-import db from './firebaseConfig.js';
-import React,{useState,useEffect} from 'react';
-import './Food.css'
+import db from "./firebaseConfig.js";
+import React, { useState, useEffect } from "react";
+import { PieChart } from "react-minimal-pie-chart";
+import "./Food.css";
 
 function Food() {
+  const [info, setInfo] = useState([]);
 
- const [food,setFood]=useState([])
-  const fetchFood=async()=>{
-    const response=db.collection('foodData');
-    const data=await response.get();
-    data.docs.forEach(item=>{
-     setFood([...food,item.data()])
-    })
-  }
-  useEffect(() => {
-    fetchFood();
-  }, [])
+  // Start the fetch operation as soon as
+  // the page loads
+  window.addEventListener("load", () => {
+    Fetchdata();
+  });
 
-    return (
-       <div className="grid">
-      {
-        food && food.map(foo=>{
-          return(
-            <div className="food-container">
-            <div className="img-container"> 
-                 <img className="food_image" src={foo.img}  alt="" />
+  // Fetch the required data using the get() method
+  const Fetchdata = () => {
+    db.collection("foodData")
+      .orderBy("foodName")
+      .get()
+      .then((querySnapshot) => {
+        // Loop through the data and store
+        // it in array to display
+        querySnapshot.forEach((element) => {
+          var data = element.data();
+          setInfo((arr) => [...arr, data]);
+        });
+      });
+  };
+
+  return (
+    <div className="grid">
+      {info &&
+        info.map((foo) => {
+          return (
+            <div className="food_container">
+              <div className="img_container">
+                <img className="food_image" src={foo.img} alt="" />
+              </div>
+              <div className="data_container">
+                <h1>{foo.foodName}</h1>
+                <h4>Calories: {foo.Calories}</h4>
+                <h4>Carbohydrate: {foo.Carbohydrate} %</h4>
+                <h4>Fat: {foo.Fat} %</h4>
+                <h4>Protein: {foo.Protein} %</h4>
+              </div>
+              <div className="pie_chart">
+                <PieChart
+                  label={(props) => {
+                    return props.dataEntry.title;
+                  }}
+                  data={[
+                    {
+                      title: "Carbs",
+                      value: foo.Carbohydrate,
+                      color: "#E38627",
+                    },
+                    { title: "Fat", value: foo.Fat, color: "#C13C37" },
+                    { title: "Protein", value: foo.Protein, color: "#6A2135" },
+                  ]}
+                />
+              </div>
             </div>
-            
-            <div className="data-container">
-              <h1>{foo.foodName}</h1>
-              <h4>Calories: {foo.Calories}</h4>
-              <h4>Carbohydrate: {foo.Carbohydrate}</h4>
-              <h4>Fat: {foo.Fat}</h4>
-              <h4>Protein: {foo.Protein}</h4>
-            </div>
-             
-              
-            </div>
-          )
-        })
-      }
+          );
+        })}
     </div>
-    )
+  );
 }
 
-export default Food
+export default Food;
